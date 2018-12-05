cb865fd6ce71dbf9d19409ace97982aeed25c84d
Arkadiusz Lewicki <43863162+SqrtLewy@users.noreply.github.com>
2018-12-05 10:16:05 +0100

    Update

--- ciag.php
+++ ciag.php
@@ -7,7 +7,12 @@ Wprowadź A1:<br />
 
 Wprowadź A3:<br />
 
-<input name="a3" /><br /><br />
+<input name="a3" /><br />
+
+Ile wyrazów ciągu wyświetlić?<br />
+
+<input name="k" /><br /><br />
+
 
 <input type="submit" value="Oblicz!" /> <br /> <br />
 </form>
@@ -22,7 +27,7 @@ $a122=$a1+($n-1)*$r;
 $n=121;
 $a121=$a1+($n-1)*$r;
 
-for( $n = 1; $n <= 10; $n++ )
+for( $n = 1; $n <= $_POST["k"]; $n++ )
    echo $an=$a1+($n-1)*$r."<br>";
 
 	echo "<br>";
@@ -32,5 +37,11 @@ echo "Różnica między elementami: $r";
 echo "A122=$a122";
 	echo "<br>";
 echo "A121=$a121";
+	echo "<br>";
+echo "Pierwszy wyraz ciągu:$a1";
+	echo "<br>";
+echo "Trzeci wyraz ciągu:$a3";
+	echo "<br>";
+echo "Różnica ciągu: $r";
 ?>
 </html>
