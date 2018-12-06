fca0aaf82ffd72bb37c42423e9824cbf934dbdc3
Szymon Ciompała <ZawiszaCzarny52@gmail.com>
2018-12-06 14:43:56 +0100

    Update ciag.php

--- ciag.php
+++ ciag.php
@@ -1,31 +1,26 @@
 <!doctype html>
-
 <form action="ciag.php" method="post">
 Wprowadź A1:<br />
-
 <input name="a2" /><br />
-
-Wprowadź A3:<br />
-
+Wprowadź A2:<br />
 <input name="a3" /><br />
-
 Ile wyrazów ciągu wyświetlić?<br />
-
 <input name="k" /><br /><br />
-
-
 <input type="submit" value="Oblicz!" /> <br /> <br />
 </form>
 
 <?php
-
-$a1=$_POST["a2"];
-$a3=$_POST["a3"];
+if (isset($_POST["a2"])) {
+	$a1=$_POST["a2"];
+}
+if (isset($_POST["a3"])) {
+	$a3=$_POST["a3"];
+}
+if (isset($_POST["a2"]) && isset($_POST["a3"]) && isset($_POST['k'])) {
 $r=($a1+$a3)/2;
-$n=122;
-$a122=$a1+($n-1)*$r;
-$n=121;
-$a121=$a1+($n-1)*$r;
+$n=$_POST['k'];
+$a122=$a1+($n--)*$r;
+$a121=$a1+($n--)*$r;
 
 for( $n = 1; $n <= $_POST["k"]; $n++ )
    echo $an=$a1+($n-1)*$r."<br>";
@@ -40,8 +35,9 @@ echo "A121=$a121";
 	echo "<br>";
 echo "Pierwszy wyraz ciągu:$a1";
 	echo "<br>";
-echo "Trzeci wyraz ciągu:$a3";
+echo "Drugi wyraz ciągu:$a3";
 	echo "<br>";
 echo "Różnica ciągu: $r";
+}
 ?>
 </html>
